9e0258c6b57b2922a6ba5e7d80dbe936a8528b72
ThuyNguyen99 <thuy4111909@gmail.com>
2022-11-09 09:04:27 +0700

    Du an hoan thanh

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,27 @@
+<template>
+    <Form>
+        <div class="form-group">
+            <label for="email"><b><i class="fa-solid fa-envelope"></i> Email</b></label>
+            <input type="email" name="email" value="thuyb1910151@gmail.com" class="form-control">
+            <ErrorMessage name="email" class="error-feedback" />
+        </div>
+        <div class="form-group">
+            <label for="password"><b><i class="fa-solid fa-plus"></i> Password</b></label>
+            <input type="password" name="password" value="12345" class="form-control">
+            <ErrorMessage name="password" class="error-feedback" />
+        </div>
+        
+
+        <div class="form-group">
+            <router-link :to="{ name: 'userbook' }" class="nav-link">
+                <button class="t-btn btn btn-info ">Đăng Nhập</button>
+            </router-link>
+        </div>
+    </Form>
+</template>
+
+
+
+<style scoped>
+@import "@/assets/form.css";
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="page t-edit my-3">
+        <h4 class="text-center">Đăng Nhập</h4>
+        <LoginForm
+        @submit:login="login"
+        />
+    </div>
+</template>
+
+<script>
+import LoginForm from "@/components/LoginForm.vue";
+//import { parseStringStyle } from "@vue/shared";
+
+export default {
+    components: {
+        LoginForm,
+    },
+    // data() {
+    //     const FixedUser = {
+    //         email: "thuyb1910151@gmail.com",
+    //         password: "12345"
+    //     }
+    // },
+    // methods: {
+    //     async login(data) {
+    //         const user = FixedUser
+    //         if (email==user.email && password==user.password){
+    //             try{
+    //                 this.$router.push({ name: "postbook" });
+    //             }  
+    //             catch (error) {
+    //                 console.log(error);
+    //             }
+    //         }
+    //     },
+    // },
+}
+</script>
